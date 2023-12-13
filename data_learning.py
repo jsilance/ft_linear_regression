f34d04fae9798af6b3bd89d16c46588611620076
jsilance <juliensilance@hotmail.com>
2023-12-13 21:44:00 +0100

    maj

--- data_learning.py
+++ data_learning.py
@@ -1,37 +1,44 @@
-# # from matplotlib import pyplot as plt
 import csv
 
 def estimate_price(mileage, theta):
 	return theta[0] + theta[1] * mileage
 
 def sumEstimateA(data, theta):
-	sum = 0
+	sum_value = 0
 	for x, y in data:
-		sum += (estimate_price(x, theta) - y) * x
-	return sum
+		sum_value += (estimate_price(x, theta) - y) * x
+	return sum_value
 
 def sumEstimateB(data, theta):
-	sum = 0
+	sum_value = 0
 	for x, y in data:
-		sum += estimate_price(x, theta) - y
-	return sum
+		sum_value += estimate_price(x, theta) - y
+	return sum_value
 
 def update_theta(theta, data, learning_rate):
 	tmp_theta = [0, 0]
 	m = len(data)
-	for x, y in data:
-		tmp_theta[0] += (estimate_price(x, theta) - y)
-		tmp_theta[1] += (estimate_price(x, theta) - y) * x
-	# tmp_theta[0] = theta[0] - learning_rate * (1 / m) * sumEstimateB(data, theta)
-	# tmp_theta[0] = learning_rate * (1 / m) * sumEstimateB(data, theta)
-	# tmp_theta[1] = theta[1] - learning_rate * (1 / m) * sumEstimateA(data, theta)
-	# tmp_theta[1] = learning_rate * (1 / m) * sumEstimateA(data, theta)
-	tmp_theta[0] *= (1 / m)
-	tmp_theta[1] *= (1 / m)
+	# for x, y in data:
+	#     tmp_theta[0] += (estimate_price(x, theta) - y)
+	#     tmp_theta[1] += (estimate_price(x, theta) - y) * x
+	
+	# tmp_theta[0] *= (1 / m)
+	# tmp_theta[1] *= (1 / m)
 
-	theta[0] -= learning_rate * tmp_theta[0]
-	theta[1] -= learning_rate * tmp_theta[1]
-	return theta
+	# theta[0] -= learning_rate * tmp_theta[0]
+	# theta[1] -= learning_rate * tmp_theta[1]
+	tmp_theta[0] = theta[0] - learning_rate * (1 / m) * sumEstimateB(data, theta)
+	tmp_theta[1] = theta[1] - learning_rate * (1 / m) * sumEstimateA(data, theta)
+
+	return tmp_theta
+
+def descent_grad(theta, data):
+	J = 0
+	m = len(data)
+	for x, y in data:
+		J += (estimate_price(x, theta) - y) ** 2
+	J = (1 / (2 * m)) * J
+	return J
 
 with open('data.csv', 'r') as file:
 	reader = csv.reader(file)
@@ -40,7 +47,10 @@ with open('data.csv', 'r') as file:
 	data = [(float(el[0]), float(el[1])) for el in data]
 	theta = [0, 0]
 	learning_rate = 0.01
-	for _ in range(5):
+	num_iterations = 1000
+	for _ in range(num_iterations):
 		theta = update_theta(theta, data, learning_rate)
-	print("Theta B: ", theta[0])
-	print("Theta A: ", theta[1])
+		cost = descent_grad(theta, data)
+		print("Theta B:", theta[0])
+		print("Theta A:", theta[1])
+		print("Cost:", cost)
